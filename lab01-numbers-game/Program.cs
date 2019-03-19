5d4a8e5a6d394c6991402a95e7f2c7da90370f87
Andrew Curtis <curtis.andrewm@gmail.com>
2019-03-19 10:20:43 -0700

    Add *real* initial work on StartSequence and Populate methods

--- lab01-numbers-game/Program.cs
+++ lab01-numbers-game/Program.cs
@@ -23,7 +23,10 @@ namespace lab01_numbers_game
             Console.WriteLine("Please enter an integer greater than zero.");
 
             // Take in user's initial number choice as string
-            string firstUserNumberAsString = Console.ReadLine(); // TODO handle invalid format exception in case user enters invalid char or number (non-int, neg)
+            string firstUserNumberAsString = Console.ReadLine(); 
+            
+            // TODO handle invalid format exception in case user enters invalid char or number (non-int, neg)
+
 
             // Convert user's initial number choice to int
             int firstUserNumberAsInt = Convert.ToInt32(firstUserNumberAsString);
@@ -31,13 +34,30 @@ namespace lab01_numbers_game
             // Declare array of length of user's initial number
             int[] userArray = new int[firstUserNumberAsInt];
 
-            // Loop to get user to enter a number for each element of array
-            for (int i = 1; i <= firstUserNumberAsInt; i++)
-            {
-                Console.WriteLine($"Please enter number {i} of {firstUserNumberAsInt}");
-                // Add current number to user's array (using Populate method?)
+            // Store populated int array as variable
+            int[] populatedArray = Populate(userArray);
 
+        }
+
+        static int[] Populate(int[] intArray)
+        {
+            // Create empty string array to store user's input
+            string[] userStringArray = new string[intArray.Length];
+
+            // Loop to get user to enter a number for each element of array
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine($"Please enter number {i + 1} of {intArray.Length}");
+                string userNumAsString = Console.ReadLine(); // TODO handle invalid format of user input
+
+                // Add user input to current index of string array
+                userStringArray[i] = userNumAsString;
             }
+        }
+
+        static int GetSum(int[] intArray)
+        {
+
         }
     }
 }
