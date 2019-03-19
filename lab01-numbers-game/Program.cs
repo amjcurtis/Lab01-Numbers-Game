1907378a3f7146be42d36549361e99ddc0b38f33
Andrew Curtis <curtis.andrewm@gmail.com>
2019-03-19 11:13:27 -0700

    Add further progress on StartSequence, Populate, GetSum, GetProduct methods (math operations work but no exception handling yet)

--- lab01-numbers-game/Program.cs
+++ lab01-numbers-game/Program.cs
@@ -6,25 +6,31 @@ namespace lab01_numbers_game
     {
         static void Main(string[] args)
         {
-            try
-            {
+            StartSequence();
 
-            }
-            catch (Exception)
-            {
 
-                throw;
-            }
+            //try
+            //{
+
+            //}
+            //catch (Exception)
+            //{
+
+            //    throw;
+            //}
+
+            Console.ReadLine();
         }
 
         static void StartSequence()
         {
             Console.WriteLine("Let's do some math!");
             Console.WriteLine("Please enter an integer greater than zero.");
+            Console.WriteLine(" ");
 
             // Take in user's initial number choice as string
-            string firstUserNumberAsString = Console.ReadLine(); 
-            
+            string firstUserNumberAsString = Console.ReadLine();
+
             // TODO handle invalid format exception in case user enters invalid char or number (non-int, neg)
 
 
@@ -37,27 +43,70 @@ namespace lab01_numbers_game
             // Store populated int array as variable
             int[] populatedArray = Populate(userArray);
 
+            int sumOfArray = GetSum(populatedArray);
+
+            int product = GetProduct(populatedArray, sumOfArray);
+
+            Console.WriteLine($"product is: {product}");
         }
 
         static int[] Populate(int[] intArray)
         {
-            // Create empty string array to store user's input
-            string[] userStringArray = new string[intArray.Length];
+            // Create empty int array to store user's input
+            int[] userIntArray = new int[intArray.Length];
 
             // Loop to get user to enter a number for each element of array
             for (int i = 0; i < intArray.Length; i++)
             {
-                Console.WriteLine($"Please enter number {i + 1} of {intArray.Length}");
+                Console.WriteLine($"Please enter number {i + 1} of {intArray.Length}.");
                 string userNumAsString = Console.ReadLine(); // TODO handle invalid format of user input
 
+                int userNumAsInt = Convert.ToInt32(userNumAsString);
+
                 // Add user input to current index of string array
-                userStringArray[i] = userNumAsString;
+                userIntArray[i] = userNumAsInt;
             }
+            return userIntArray;
         }
 
         static int GetSum(int[] intArray)
         {
+            // Set counter
+            int sum = 0;
 
+            // Sum elements of array
+            foreach (int element in intArray)
+            {
+                sum += element;
+            }
+
+            // TODO Add ability to throw custom exception is sum < 20
+            // Error message should be: "$Value of {sum} is too low"
+
+            Console.WriteLine($"The sum of the numbers you entered is {sum}.");
+            return sum;
+        }
+
+        static int GetProduct(int[] intArray, int sum)
+        {
+            Console.WriteLine($"Please pick a random number between 1 and {intArray.Length}.");
+
+            // Store user's random number as variable
+            string userPickAsString = Console.ReadLine();
+
+            // Convert user pick to int
+            int userPickAsInt = Convert.ToInt32(userPickAsString); // TODO handle invalid format of user input
+
+            // TODO Handle IndexOutOfRange exception
+                // Output message to console
+                // Throw exception back down to Main
+
+
+            // Multiply sum argument by array element at index corresponding to user's random number
+            int product = intArray[userPickAsInt] * sum;
+
+            Console.WriteLine($"The product of the number at the random index you picked multiplied by the sum of your array of numbers is {product}.");
+            return product;
         }
     }
 }
